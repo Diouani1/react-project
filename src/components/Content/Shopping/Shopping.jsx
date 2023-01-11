a3e44c2e126a8ee8075a6c99071c235d8487aee3
Diouani <diouanijalouka@gmail.com>
2023-01-11 21:24:43 +0100

    updating shopping component

--- src/components/Content/Shopping/Shopping.jsx
+++ src/components/Content/Shopping/Shopping.jsx
@@ -62,66 +62,65 @@ function DeletItem  (e) {
 };
   
   return (
-    <Container style={{ padding:"1rem"}} fluid>
-      <Row>
-        <Col xs={12} sm={10}>
-          <div className='shoppingList'>
-
-            {products && products.map((item, index) => (
-            
-                <Button className='shoppingButton'
-                  color="primary"
-                  key={index}
-                  onClick={() => handelSum(item)}
-                  >
-                  <h6>{item.name}</h6><span>{item.size}</span>  <br /><span>{item.price}  Euros</span>
-                </Button>
-              
-            ))}
-        
-            </div>
-        </Col>
-        <Col xs={4} sm={2}>
-        {/* <Col> */}
-          <div className="priceList">
-            <div className="table">
-              <Table dark striped >
-                <thead>
-                  <tr>
-                    <th></th>
-                    <th>Product</th>
-                    <th>Price</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {sum.map((item, index) => (
-                    <tr key={index}  onClick={DeletItem} style={{ cursor:"pointer" }} name={index}>
-                      <th scope="row">{index + 1}</th>
     
-                      <td >{item.name}</td>
-                      <td>{item.price}</td>
+      <Container style={{ padding:"1rem"}} fluid>
+        <Row>
+          <Col xs={12} sm={9}>
+            <div className='shoppingList'>
+              {products && products.map((item, index) => (
+      
+                  <Button className='shoppingButton'
+                    color="primary"
+                    key={index}
+                    onClick={() => handelSum(item)}
+                    >
+                    <h6>{item.name}</h6><span>{item.size}</span>  <br /><span>{item.price}  Euros</span>
+                  </Button>
+      
+              ))}
+      
+              </div>
+          </Col>
+          <Col>
+          
+            <div className="priceList">
+              <div className="table">
+                <Table dark striped >
+                  <thead>
+                    <tr>
+                      <th></th>
+                      <th>Product</th>
+                      <th>Price</th>
                     </tr>
-                  ))}
-                </tbody>
-              </Table>
+                  </thead>
+                  <tbody>
+                    {sum.map((item, index) => (
+                      <tr key={index}  onClick={DeletItem} style={{ cursor:"pointer" }} name={index}>
+                        <th scope="row">{index + 1}</th>
+      
+                        <td >{item.name}</td>
+                        <td>{item.price}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </div>
+              <div className="sum">
+                <span>Sum</span>
+                <span>
+                  {sum.reduce((prev, curr) => prev + +curr.price, 0).toFixed(2)}
+                </span>
+              </div>
+              <ButtonGroup vertical style={{width:"100%",display: "block"}}>
+                <Button color="danger" onClick={handelAllDelete}>Delete All Products</Button>
+                <NavLink to="paid" >
+          <Button color="success">Done</Button>
+        </NavLink>
+              </ButtonGroup>
             </div>
-            <div className="sum">
-              <span>Sum</span>
-              <span>
-                {sum.reduce((prev, curr) => prev + +curr.price, 0).toFixed(2)}
-              </span>
-            </div>
-            <ButtonGroup vertical style={{width:"100%",display: "block"}}>
-              <Button color="danger" onClick={handelAllDelete}>Delete All Products</Button>
-              <NavLink to="paid" > 
-        <Button color="success">Done</Button>
-      </NavLink>
-            </ButtonGroup>
-          </div>
-        </Col>
-      </Row>
-
-    </Container>
+          </Col>
+        </Row>
+      </Container>
   );
 };
 
